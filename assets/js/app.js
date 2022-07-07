99ba5da84e0c10b2159d3892e96b6653ff2d1103
Musharrof Shishir <musharrofshishir@gmail.com>
2022-07-07 08:32:03 +0600

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+new Splide( '.splide', {
+    type   : 'loop',
+    perPage: 3,
+    perMove: 1,
+    width: 'calc(90% - 20px)',
+    fixedWidth: '340px',
+    fixedHeight: '400px',
+    gap: 38,
+    pagination: false,
+  } ).mount();
+// new Splide( '.splide', {
+//     type   : 'loop',
+//     perPage: 3,
+//   } );
