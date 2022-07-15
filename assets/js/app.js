5abd473e1b621df73be10c684dd7f753a2cb95aa
Musharrof Shishir <musharrofshishir@gmail.com>
2022-07-15 14:59:15 +0600

    working

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,3 +12,29 @@ new Splide( '.splide', {
 //     type   : 'loop',
 //     perPage: 3,
 //   } );
+
+document.addEventListener( 'DOMContentLoaded', function () {
+  var main = new Splide( '#main-carousel', {
+    type      : 'loop',
+    rewind    : true,
+    pagination: false,
+    arrows    : false,
+  } );
+
+  var thumbnails = new Splide( '#thumbnail-carousel', {
+    fixedWidth  : 100,
+    gap         : 10,
+    rewind      : true,
+    pagination  : false,
+    isNavigation: true,
+    autoplay: 'play',
+    pauseOnHover: false,
+    pauseOnFocus: false,
+  } );
+
+  main.sync( thumbnails );
+  main.mount();
+  thumbnails.mount();
+} );
+
+// GSAP
