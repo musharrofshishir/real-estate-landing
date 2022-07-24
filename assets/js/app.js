40df09917cbefb4b4f3be4eeff2cca34b75d6308
Musharrof Shishir <musharrofshishir@gmail.com>
2022-07-24 10:42:50 +0600

    working

--- assets/js/app.js
+++ assets/js/app.js
@@ -43,39 +43,39 @@ let tl = gsap.timeline();
 
 tl.from('._body',{
   opacity:0,
-  duration:1,
+  duration:.5,
   ease: "power3.out"
 })
 .from('._real_header_content_title',{
   y:100,
   opacity: 0,
-  duration:.5,
+  duration:.3,
   ease: "back.out(2)"
 })
 .from('._real_header_content_p',{
   y:100,
   opacity: 0,
-  duration:.5,
+  duration:.3,
   ease: "back.out(1.7)"
-},1)
+},.6)
 .from('._real_header_content_btn_wrap',{
   y:100,
   opacity: 0,
-  duration:.5,
+  duration:.3,
   ease: "back.out(1.8)"
-},1.2)
+},.8)
 .from('._real_header_content_support_inner',{
   y:100,
   opacity: 0,
-  duration:.5,
+  duration:.3,
   ease: "back.out(1.8)"
-},1.2)
+},1)
 .from('._real_header_img',{
   y:50,
   opacity: 0,
-  duration:.5,
+  duration:.3,
   ease: "back.out(1.8)"
-},1.2)
+},1)
 .from('._real_header_content_lock',{
   scale:0,
   opacity: 0,
@@ -83,19 +83,19 @@ tl.from('._body',{
   ease: "back.out(1.8)"
 })
 .from('._real_header_img_trust',{
-  y:50,
+  scale:0,
   opacity: 0,
   duration:.2,
   ease: "back.out(1.8)"
 })
 .from('._real_header_img_mortgage',{
-  y:50,
+  scale:0,
   opacity: 0,
   duration:.2,
   ease: "back.out(1.8)"
 })
 .from('._real_header_img_trophy',{
-  y:50,
+  scale:0,
   opacity: 0,
   duration:.2,
   ease: "back.out(1.8)"
@@ -105,42 +105,69 @@ tl.from('._body',{
   scale:.9,
   rotate:-120,
   opacity: 0,
-  duration:.5,
+  duration:.3,
   ease: "back.out(1.8)"
 })
 .from('._real_header_img_shape1',{
   y:-20,
   opacity: 0,
-  duration:1,
+  duration:.5,
   ease: "power3.in"
-},1.8)
+},1.2)
 .from('._real_header_shape',{
   y:-20,
   opacity: 0,
-  duration:1,
+  duration:.8,
   ease: "power3.in"
-},1.8)
+},1.2)
 .from('._real_header_content_shape1',{
   opacity: 0,
-  duration:1,
+  duration:.8,
   ease: "power3.in"
-},1.3)
+},1)
 .from('._real_header_content_shape2',{
   opacity: 0,
-  duration:1,
+  duration:.8,
   ease: "power3.in"
-},1.5)
+},1.2)
 .from('._real_header_content_shape3',{
   opacity: 0,
-  duration:1,
+  duration:.8,
   ease: "power3.in"
-},1.8)
+},1.3)
 .from('._real_header_nav_wrap',{
   y: -200,
-  duration:1.4,
+  duration:1,
   ease: "power3.out"
-},1.8)
-
+},1.2)
+gsap.from('._real_header_content_lock',{
+  scrollTrigger:{
+    trigger:'._real_header_img_wrap',
+    scrub:1
+  },
+  y: 100
+})
+gsap.from('._real_header_img_trust',{
+  scrollTrigger:{
+    trigger:'._real_header_img_wrap',
+    scrub:.2
+  },
+  y: 100
+})
+gsap.from('._real_header_img_mortgage',{
+  scrollTrigger:{
+    trigger:'._real_header_img_wrap',
+    scrub:.2
+  },
+  y: 100
+})
+gsap.from('._real_header_img_trophy',{
+  scrollTrigger:{
+    trigger:'._real_header_img_wrap',
+    scrub:.2
+  },
+  y: 100
+})
 gsap.from('._real_avoid_wrap_shape',{
   scrollTrigger:{
     trigger: '._real_avoid_wrap_bg',
@@ -236,7 +263,43 @@ gsap.fromTo('._real_marque_content_ul',{
   ease: "linear",
   repeat: -1
 });
-
+gsap.from('._real_marque_shape',{
+  scrollTrigger:{
+    trigger:'._real_marque_wrapper',
+  },
+  y:80,
+  x:-100,
+  opacity: 0,
+  duration:2,
+  ease: "power3.out"
+})
+gsap.from('._real_testimonial_img',{
+  scrollTrigger:{
+    trigger:'._real_testimonial_content_img',
+  },
+  y: 50,
+  opacity: 0,
+  duration:1.2,
+  ease: "power3.out"
+})
+gsap.from('._real_testimonial_content_img_inner',{
+  scrollTrigger:{
+    trigger:'._real_testimonial_content_img',
+  },
+  y: -50,
+  opacity: 0,
+  delay: .5,
+  duration:1.6,
+  ease: "power3.out"
+})
+gsap.from('._real_testimonial_content_img_bg',{
+  scrollTrigger:{
+    trigger:'._real_testimonial_content_img',
+  },
+  width: 0,
+  duration:1.8,
+  ease: "power3.out"
+})
 
 
 
